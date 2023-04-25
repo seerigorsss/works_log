93c960ce376f4c1a15e4328e8e16d9c3b96fe0de
Igor Sirotkin <myEmail@example.com>
2023-04-25 19:22:50 +0500

    Решение задачи "Форма регистрации"

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
-from data.jobs import Jobs
+from forms.user import RegisterForm
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -11,11 +12,28 @@ def main():
     app.run(port=8080, host='127.0.0.1')
 
 
-@app.route("/")
-def index():
-    db_sess = db_session.create_session()
-    jobs = db_sess.query(Jobs).all()
-    return render_template('index.html', jobs=jobs)
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
